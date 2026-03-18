96158c768ce9114f56d2ab052dad5dcf148473dd
Mike Schall <mikes@a-t-g.com>
2026-03-18 11:49:58 -0500

    fix: restore Prettier configuration options in prettier.config.js

--- packages/plugin-tsql/prettier.config.js
+++ packages/plugin-tsql/prettier.config.js
@@ -1,6 +1,12 @@
 export default {
-    plugins: ['./dist/index.js'],
-    sqlKeywordCase: 'lower',
-    sqlDensity: 'standard',
-    sqlCommaStyle: 'trailing',
+    plugins: ["./dist/index.js"],
+    sqlKeywordCase: "lower",
+    sqlDensity: "standard",
+    sqlCommaStyle: "trailing",
+
+    // standard prettier options
+    printWidth: 120,
+    tabWidth: 4,
+    singleQuote: true,
+    trailingComma: "all",
 };
